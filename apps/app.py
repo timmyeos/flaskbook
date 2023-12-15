790f56d3fb721e640ed19d3c4ab4940066bcd412
timmyeos <97524127+timmyeos@users.noreply.github.com>
2023-12-15 16:51:31 +0900

    Update app.py
    <Chapter 03 사용자 인증 기능 만들기>
    flask-login과 연계, Blueprint로 인증 기능 등록

--- apps/app.py
+++ apps/app.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
@@ -12,6 +14,13 @@ db = SQLAlchemy()
 # 117 page
 csrf = CSRFProtect()
 
+# 149 page: LoginManager를 인스턴스화 한다
+login_manager = LoginManager()
+# login_view 속성에 미로그인 시 회원가입 기능으로 리다이렉트하는 엔드포인트를 지정한다
+login_manager.login_view = "auth.signup"
+# login_message 속성에 로그인 후에 표시할 메세지 지정
+login_manager.login_message = ""
+
 
 # 90 page: create_app 함수를 작성한다
 # 139 page: config의 키를 전달한다
@@ -19,10 +28,10 @@ def create_app(config_key):
     # flask 인스턴스 생성
     app = Flask(__name__)
 
+    ### database ###
     # config_key에 매치하는 환경의 config 클래스를 읽어들인다
     app.config.from_object(config[config_key])
 
-    # database
     # # 100 page. 앱의 config 설정을 한다
     # app.config.from_mapping(
     #     SECRET_KEY="1234",
@@ -42,11 +51,20 @@ def create_app(config_key):
     # Migrate와 앱을 연계한다
     Migrate(app, db)
 
-    # blueprint
+    # login_manager를 앱과 연계한다
+    login_manager.init_app(app)
+
+    ### blueprint ###
     # apps폴더 밑의 crud 패키지로부터 views.py를 import 한다
     from apps.crud import views as crud_views
 
     # register_blueprint를 사용해 views의 crud를 앱에 등록한다
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
+
+    # 앞으로 작성하는 apps폴더 밑의 auth 패키지로부터 views.py를 import 한다
+    from apps.auth import views as auth_views
+
+    # register_blueprint를 사용해 views의 auth를 앱에 등록한다
+    app.register_blueprint(auth_views.auth, url_prefix="/auth")
 
     return app
